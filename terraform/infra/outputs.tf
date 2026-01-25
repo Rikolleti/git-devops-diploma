c9f22456cd306445a9d240138a3b8517ce1852ad
Azhdar Mamedov <you@example.com>
2026-01-25 17:33:31 +0300

    Fix outputs for counted VM

--- terraform/infra/outputs.tf
+++ terraform/infra/outputs.tf
@@ -1,5 +1,6 @@
 output "vm_public_ip" {
-  value = yandex_compute_instance.vm1.network_interface[0].nat_ip_address
+  description = "Public IP of compute VM (null if VM is disabled)"
+  value       = try(yandex_compute_instance.vm1[0].network_interface[0].nat_ip_address, null)
 }
 
 output "k8s_cluster_name" {
