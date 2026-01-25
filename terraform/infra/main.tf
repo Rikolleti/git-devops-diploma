1ab82f8763aac44fa3720b0e361ed26c2b2e1bf0
Azhdar Mamedov <you@example.com>
2026-01-25 17:47:14 +0300

    Added VM again

--- terraform/infra/main.tf
+++ terraform/infra/main.tf
@@ -33,10 +33,8 @@ resource "yandex_vpc_subnet" "public_d" {
   v4_cidr_blocks = ["192.168.12.0/24"]
 }
 
-#
 # ---------- VM ----------
 resource "yandex_compute_instance" "vm1" {
-  count       = 0
   name        = var.vm_name
   platform_id = "standard-v1"
   zone        = var.zone
